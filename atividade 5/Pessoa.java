6c17f76197b19689f184613e28148edfde13454e
Gabriel-Angelo-Silva <136750547+Gabriel-Angelo-Silva@users.noreply.github.com>
2023-09-13 14:49:23 -0400

    Create Pessoa.java

---
+++ atividade 5/Pessoa.java
@@ -0,0 +1,42 @@
+public abstract class Pessoa {
+    private String nome, genero;
+    private int idade;
+
+    public Pessoa(String nome, int idade, String genero) {
+        this.nome = nome;
+        this.idade = idade;
+        this.genero = genero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public abstract void realizarAtividade();
+
+    public void apresentar() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Gênero: " + genero);
+    }
+}
