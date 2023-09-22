696ff969ebebc9fa55d1b27320d9c7a03ed80883
Gabriel-Angelo-Silva <136750547+Gabriel-Angelo-Silva@users.noreply.github.com>
2023-09-22 14:41:34 -0400

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa();
-		p1.nome = "Gabriel";
-		p1.ValorEmConta = 0;
-		p1.Banco = "Inter";
-		p1.ExibirDados(p1.nome,p1.Banco,p1.ValorEmConta);
-	}
-}
