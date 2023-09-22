d719c61131cc735fbf26aa83b0100a49afad99a0
Gabriel-Angelo-Silva <136750547+Gabriel-Angelo-Silva@users.noreply.github.com>
2023-09-22 14:50:43 -0400

    Update Main.java

--- atividade 1/Main.java
+++ atividade 1/Main.java
@@ -1,10 +1,20 @@
 public class Main
 {
 	public static void main(String[] args) {
+		System.out.println("Primeira Conta:");
 		Pessoa p1 = new Pessoa();
 		p1.nome = "Gabriel";
 		p1.ValorEmConta = 0;
 		p1.Banco = "Inter";
 		p1.ExibirDados(p1.nome,p1.Banco,p1.ValorEmConta);
+		
+		System.out.println();
+		
+		System.out.println("Segunda Conta:");
+		Pessoa p2 = new Pessoa();
+		p2.nome = "Bolsonaro";
+		p2.ValorEmConta = 1000000000;
+		p2.Banco = "SuiçaCorp";
+		p2.ExibirDados(p2.nome,p2.Banco,p2.ValorEmConta);
 	}
 }
