4b3231a63ef349bd0df942a12fe0d4e71950774e
Gabriel-Angelo-Silva <136750547+Gabriel-Angelo-Silva@users.noreply.github.com>
2023-10-06 12:02:18 -0400

    Update Pessoa.java

--- atividade 4/Pessoa.java
+++ atividade 4/Pessoa.java
@@ -1,7 +1,6 @@
 public class Pessoa {
-    private String nome;
+    private String genero, nome;
     private int idade;
-    private String genero;
 
     public Pessoa(String nome, int idade, String genero) {
         this.nome = nome;
